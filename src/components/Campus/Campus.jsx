ef9cb355e35de596ea6814396c2ebfd1daa20e28
dernouni_mk <mohameddernouni13@gmail.com>
2025-01-30 10:45:40 +0100

    first commit

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -11,13 +11,25 @@ import icon from "../../assets/white-arrow.png"
 function Campus() {
   return (
     <div className='gallery container'id='campus'>
-        <div className="images">
+        <div className="images row">
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_1} alt="" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_2} alt="" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_3} alt="" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_4} alt="" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_5} alt="" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
             <img src={gallery_6} alt="" />
+          </div>
         </div>
         <div className="more">
             <p>See more here</p>
